cce34e0a7d7da2fc248a81fdda36dab4bd930fd6
katplee <katpelee@gmail.com>
2022-02-13 18:28:27 +0900

    Main menu implementation

---
+++ Weekly Game Jam - Week 171/Assets/Scripts/UI/Improved/OpeningScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpeningScene : MonoBehaviour
+{
+    private int mainMenu = 0;
+    private int pauseMenu = 1;
+    private int prototype = 2;
+
+    public void Play()
+    {
+        SceneManager.LoadScene(prototype);
+        Time.timeScale = 1f;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
