9023b23eaa0d0d1c7f06c3cd871e7d386cb3cd3b
katplee <katpelee@gmail.com>
2022-02-13 14:50:44 +0900

    Pause menu implementation

---
+++ Weekly Game Jam - Week 171/Assets/Scripts/UI/Improved/PauseScene.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScene : MonoBehaviour
+{
+    private int mainMenu = 0;
+    private int pauseMenu = 1;
+    private int prototype = 2;
+
+    public void Resume()
+    {
+        SceneManager.LoadScene(prototype);
+        Time.timeScale = 1f;
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(mainMenu);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
